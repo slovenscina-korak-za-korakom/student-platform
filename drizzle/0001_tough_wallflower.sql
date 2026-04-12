42d9249918e984398469080160fe6ee0eb1666e4
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2026-04-11 18:54:37 -0700

    remove /stripts and /drizzle from git

--- drizzle/0001_tough_wallflower.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "lang_club" ADD COLUMN "peopleBooked" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "lang_club" ADD COLUMN "maxBooked" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "lang_club" DROP COLUMN "maxPeople";
